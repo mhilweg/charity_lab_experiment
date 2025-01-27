54ea282ecda018885a6feab7f586083b0a8c4fd0
michael hilweg <michael.hilweg@gess.uni-mannheim.de>
2025-01-27 12:07:02 +0100

    added runtime.txt, small changes in other files

--- settings.py
+++ settings.py
@@ -1,4 +1,12 @@
 from os import environ
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+}
+
+# Allow Heroku to serve your app
+ALLOWED_HOSTS = ['*']
 
 SESSION_CONFIGS = [
     {
